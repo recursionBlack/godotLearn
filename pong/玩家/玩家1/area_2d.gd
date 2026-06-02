8e8fed920ff7bff1073371c2a2d6c49086d5f90e
Kuan <recursionBlack@163.com>
2026-06-02 09:29:17 +0800

    12-GDScript简单入门——信号与await

--- pong/玩家/玩家1/area_2d.gd
+++ pong/玩家/玩家1/area_2d.gd
@@ -1,6 +1,17 @@
 extends Area2D
 
+signal abc
 
+func _ready() -> void:
+	# await 用法
+	var time = get_tree().create_timer(3)
+	await time.timeout
+	# 准备阶段，将信号与槽函数绑定
+	self.abc.connect(a)
+	abc.emit(12)
+	
+func a(b):
+	print(b)
 func _physics_process(delta: float) -> void:
 	for i in get_overlapping_areas():
 		if i.is_in_group("Ball"):
@@ -14,3 +25,9 @@ func _physics_process(delta: float) -> void:
 		if y3 < 630:
 			self.position.y = self.position.y - y1 + y2
 	pass
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Ball"):
+		area.vec.x = 5
+	pass # Replace with function body.
